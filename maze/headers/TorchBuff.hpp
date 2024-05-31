c6861ef94c0fbb52f6ff4df65a331a3cf0f109e0
EduardRK <Eduard.ivanushkin.100@gmail.com>
2024-05-31 16:36:32 +0300

    Add buff draw

--- maze/headers/TorchBuff.hpp
+++ maze/headers/TorchBuff.hpp
@@ -6,18 +6,22 @@
 class TorchBuff final : public Buff
 {
 private:
+    static constexpr float GAP = 0.02f;
+    static constexpr float COLOR_GAP = (255.f / 30.f) / 255.f;
+
     static constexpr int BUFF_BONUS_DAMAGE = 2;
     static constexpr int BUFF_RADIUS_OF_VIEW = 3;
 
     Player &_player;
 
     int _turnsLeft = 30;
+    float _redColor = 1.f;
 
 public:
     TorchBuff(Player &player);
     ~TorchBuff() = default;
 
-    void draw() override;
+    void draw(int buffIndex, float side) override;
     void nextTurn() override;
     void attack() override;
     bool complete() override;
